a54ff26d1be204d37491a244006fee8ed800b4be
Rajat Garg <rajatgarg7630@gmail.com>
2019-09-30 07:53:05 +0530

    Updates

---
+++ precache-manifest.ca36fa9ac46eda2f06c2719a0aaafe2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d998c3f19085fefe71e783c6e113fb",
+    "url": "/admin-conflu/index.html"
+  },
+  {
+    "revision": "ef16efe6ad10d1e39ac8",
+    "url": "/admin-conflu/static/css/main.bd593024.chunk.css"
+  },
+  {
+    "revision": "eba970f6229ea0e6bd78",
+    "url": "/admin-conflu/static/js/2.fe098c7f.chunk.js"
+  },
+  {
+    "revision": "ef16efe6ad10d1e39ac8",
+    "url": "/admin-conflu/static/js/main.64b142f2.chunk.js"
+  },
+  {
+    "revision": "20c51b8bf1c5d524e04e",
+    "url": "/admin-conflu/static/js/runtime~main.bf21e3a0.js"
+  }
+]);
